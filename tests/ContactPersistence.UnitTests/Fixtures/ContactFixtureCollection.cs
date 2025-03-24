4a664a4e1c3f0fccfeda4c63de088daad1c2c63d
Gabriel Ricardo <gabriel.rodrigues.ricardo@outlook.com>
2025-03-24 20:06:33 -0300

    add model entities unit tests

---
+++ tests/ContactPersistence.UnitTests/Fixtures/ContactFixtureCollection.cs
@@ -0,0 +1,6 @@
+﻿namespace ContactPersistence.UnitTests.Fixtures;
+
+[CollectionDefinition(nameof(ContactFixtureCollection))]
+public class ContactFixtureCollection : ICollectionFixture<ContactFixture>
+{
+}
